52bfe19ee5b9e67b718ff63ca7ed5396986a53b3
Priyadas <mpriyadas4@gmail.com>
2026-03-31 17:13:22 +0530

    variables created

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 Person1 = Trump
 Person2 = Musk
-echo "Person1:: Hello Person2, How are you"
-echo "Person2:: Hello Person1, Im fine How about you"
-echo "Person1:: Hello Person2, Im fine too"
-echo "Person2:: when are elections conducting, just asking"
-echo "Person1:: Probably, we are planning May 2028".
+echo "$Person1:: Hello $Person2, How are you"
+echo "$Person2:: Hello $Person1, Im fine How about you"
+echo "$Person1:: Hello $Person2, Im fine too"
+echo "$Person2:: when are elections conducting, just asking"
+echo "$Person1:: Probably, we are planning May 2028".
