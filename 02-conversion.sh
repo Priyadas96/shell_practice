ecf049fe822a5286e1887d1b5f72ce17842bbf9a
Priyadas <mpriyadas4@gmail.com>
2026-03-31 16:59:40 +0530

    hello world started

---
+++ 02-conversion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Trump:: Hello Musk, How are you"
+echo "Musk:: Hello Trump, Im fine How about you"
+echo "Trump:: Hello Musk, Im fine too"
+echo "Musk:: when are elections conducting, just asking"
+echo "Trump:: Probably, we are planning May 2028".
