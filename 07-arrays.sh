bd1902fe2d2b118d6d6da479116f31c39c034610
Priyadas <mpriyadas4@gmail.com>
2026-03-31 17:58:01 +0530

    variables modified

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-movies = ("Court" "Dhurandar" "Lucky" "GhostRider")
+movies=("Court" "Dhurandar" "Lucky" "GhostRider")
 
 echo "First movie: ${movies[0]}"
 echo "first movie: ${movies[3]}"
