3e9641edf65e0f7dc5e9b456b60803b5ce46fd01
Priyadas <mpriyadas4@gmail.com>
2026-03-31 17:47:58 +0530

    variables modified

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 Number1=100
 Number2=200
-
 Timestamp=$(date)
-echo "the time is :: $Timestamp"
+echo "script executed at :: $Timestamp"
 SUM=$(($Number1 + $Number2))
 echo "sum of $Number1 and $Number2 is :: $SUM"
