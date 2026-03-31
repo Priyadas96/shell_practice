fffe76385677a0c13d104831fc0aa3671af30ff4
Priyadas <mpriyadas4@gmail.com>
2026-03-31 17:24:09 +0530

    variables modified

---
+++ 04-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=$1
+Person2=$2
+echo "$Person1:: Hello $Person2, How are you"
+echo "$Person2:: Hello $Person1, Im fine How about you"
+echo "$Person1:: Hello $Person2, Im fine too"
+echo "$Person2:: when are elections conducting, just asking"
+echo "$Person1:: Probably, we are planning May 2028".
