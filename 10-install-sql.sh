6b91cdd238dfdb51e49bf935826733ab10667f22
Priyadas <mpriyadas4@gmail.com>
2026-04-01 16:36:06 +0530

    mysql install

--- 10-install-sql.sh
+++ 10-install-sql.sh
@@ -10,12 +10,18 @@ else
     echo "you are root access"
 fi
 
-dnf install mysql -y
-
-if [ $? -eq 0 ]
+dnf list installed mysql
+if [ $? -ne 0 ]
 then
-    echo "Success:: Installing Mysql server Success"
-else
-    echo "FAIL :: MySQL server not Installed"
+    echo "MySql is not Install...going to install"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing MySql is......Success"
+    else
+        echo "Installing Mysql is......Failure"
     exit 1
+    fi  
+else
+    echo "MySql is already Installed...nothing to do"
 fi
