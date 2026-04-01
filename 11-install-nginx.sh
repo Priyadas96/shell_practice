fd584452041122cea28a0d69a77745890da7409a
Priyadas <mpriyadas4@gmail.com>
2026-04-01 16:59:41 +0530

    mysql install

--- 11-install-nginx.sh
+++ 11-install-nginx.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
-user=$(id -u)
 
-if [ $user -ne 0 ]
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
 then
-    echo "please Login with Root access..."
+    echo "Please login with Root access."
     exit 1
 else
-    echo "you are root access"
-fi
-
-dnf list installed mysql
-if [ $? -ne 0 ]
-then
-    echo "Mysql not installed...going to install now"
-    dnf install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "MySql Install is.... Success"
-    else
-        echo "Installing Mysql is ....Failure"
-    exit 1
-    fi
-else
-    echo "Already Installed MySql ... Nothing to do"
-fi
+    echo "you are a Root User"
