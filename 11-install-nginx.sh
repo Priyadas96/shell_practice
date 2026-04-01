ce78452e3c982ff433b26f6b38e220c789ef8019
Priyadas <mpriyadas4@gmail.com>
2026-04-01 17:10:02 +0530

    mysql install

--- 11-install-nginx.sh
+++ 11-install-nginx.sh
@@ -9,3 +9,19 @@ then
 else
     echo "you are a Root User"
 fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "Nginx is not Installed...going to install now "
+    dnf install nginx -y
+    if [ $? -eq 0 ]
+    then
+        echo "Nginx installation....:: Success"
+    else
+        echo "Nginx installation.....:: Failure"
+        exit 1
+    fi
+else
+    echo "Nginx already installed...Nothing to do"
+fi
