8d709bcbdfc073cfa77dee507e9e79e64ae8fded
Priyadas <mpriyadas4@gmail.com>
2026-04-01 14:57:45 +0530

    special variables

--- 10-install-sql.sh
+++ 10-install-sql.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-result=$(id -u)
+userid=$(id -u)
 
-if [ $result -ne 0 ]
+if [ $userid -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root access"
-    exit 1
+    echo "Error:: Please login with root access"
 else
-    echo "you are running with root access"
+    echo "you are root access"
+    exit 1
 fi
 
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "Success:: Installing Mysql server Success"
+else
+    echo "FAIL :: MySQL server not Installed"
+    exit 1
+fi
