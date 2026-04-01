c41c7058b6b4bea27ad5817a2582c546cf3c6571
Priyadas <mpriyadas4@gmail.com>
2026-04-01 16:54:10 +0530

    mysql install

--- 11-install-nginx.sh
+++ 11-install-nginx.sh
@@ -8,3 +8,19 @@ then
 else
     echo "you are root access"
 fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "Mysql not installed...going to install now"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "MySql Install is.... Success"
+    else
+        echo "Installing Mysql is ....Failure"
+    exit 1
+    fi
+else
+    echo "Already Installed MySql ... Nothing to do"
+fi
