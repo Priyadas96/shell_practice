10e70e8dda6d7037a2351ae243a9ce2472f4c72d
Priyadas <mpriyadas4@gmail.com>
2026-04-02 10:44:21 +0530

    function creation

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 userid=$(id -u)
 
-if [ $userid -ne 0]
+if [ $userid -ne 0 ]
 then
-    echo "Please login with root access"
+    echo "Error :: Please login with root access"
 else
     echo "Root user loggedIn"
 fi
