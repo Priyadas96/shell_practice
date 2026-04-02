f236f9cb5d5519aa03139a5f9e58347ed3ce66de
Priyadas <mpriyadas4@gmail.com>
2026-04-02 10:50:27 +0530

    function creation

--- 12-functions.sh
+++ 12-functions.sh
@@ -4,6 +4,15 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "Error :: Please login with root access"
+    exit 1
 else
     echo "Root user loggedIn"
 fi
+
+dnf list installed mysql
+if [ $? -eq 0 ]
+then
+    echo "Info :: Mysql not available....Mysql going to be installed now "
+    exit 1
+else
+    echo "Mysql already Installed...!"
