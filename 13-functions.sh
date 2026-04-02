4bcad37367c3f9904b970d793d120dfe376f856f
Priyadas <mpriyadas4@gmail.com>
2026-04-02 11:27:18 +0530

    function creation

--- 13-functions.sh
+++ 13-functions.sh
@@ -10,6 +10,7 @@ else
     echo "Root user LoggedIn"
 fi
 
+#Mysql Installation
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
@@ -26,4 +27,38 @@ else
     echo "Mysql already installed"
 fi
 
-   
+#Python Installation
+dnf list installed python
+if [ $? -ne 0 ]
+then
+    echo "Python not available...trying to install now"
+    dnf install python -y
+    if [ $? -eq 0 ]
+    then
+        echo "Python Installation :: Success"
+    else
+        echo "Python Installation :: Failure"
+        exit 1
+    fi
+else
+    echo "Python is already installed"
+    exit 1
+fi
+
+#Nginx Installation
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "Nginx not available.. trying to Install now"
+    dnf install nginx -y
+    if [ $? -eq 0 ]
+    then
+        echo "Nginx Installation : Success"
+    else
+        echo "Nginx Installation : Failure"
+        exit 1
+    fi
+else
+    echo "Nginx already installed"
+    exit 1
+fi
