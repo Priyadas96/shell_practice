557c0ff7667120ad3a576f46994600ae0a625f70
Priyadas <mpriyadas4@gmail.com>
2026-04-22 22:46:26 +0530

    create new

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-DISK_THRESHOLD=1
+DISK_THRESHOLD=1 # in project it will be 75
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
@@ -9,8 +9,10 @@ while IFS= read line; do
 	USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
 	PARTITION=$(echo $line | awk '{print $7F}')
 	if [ $USAGE -ge $DISK_THRESHOLD ]; then
-		MSG+="High Disk Usage on $PARTITION:$USAGE \n"
+		MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
 	fi
 done <<<$DISK_USAGE
-echo -e $MSG
+
+#echo -e $MSG
+
 sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "mpriyadas30@gmail.com" "ALERT-High Disk Usage"
