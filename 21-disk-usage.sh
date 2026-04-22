b8e9e43d4cf99cd785462c8f52a9c13546eaf2ae
Priyadas <mpriyadas4@gmail.com>
2026-04-22 19:37:47 +0530

    create new

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -7,4 +7,4 @@ while IFS= read line; do
 	USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
 	PARTITION=$(echo $line | awk '{print $7F}')
 	echo "$PARTITION:$USAGE"
-done <<<DISK_USAGE
+done <<<$DISK_USAGE
