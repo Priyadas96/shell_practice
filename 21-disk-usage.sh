b71dabec5ba3ba20a09f633b1a8663de30efc293
Priyadas <mpriyadas4@gmail.com>
2026-04-22 19:55:23 +0530

    create new

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -2,9 +2,13 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1
+MSG=""
 
 while IFS= read line; do
 	USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
 	PARTITION=$(echo $line | awk '{print $7F}')
-	echo "$PARTITION:$USAGE"
+	if [ $USAGE -ge $DISK_THRESHOLD ]; then
+		MSG+="High Disk Usage on $PARTITION % <br>"
+	fi
 done <<<$DISK_USAGE
+echo -e $MSG
